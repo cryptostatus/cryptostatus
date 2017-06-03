ba62935faff9738e3fc9ba24de4b9551aae22ada
Kirill Shevchenko <kirills167@gmail.com>
2017-06-03 10:52:43 +0300

    setup deploy tools

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
+gem 'dotenv-rails', '~> 2.2'
 
 group :development, :test do
   gem 'byebug'
@@ -24,5 +25,4 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ config/deploy.rb
@@ -0,0 +1,87 @@
+lock '3.7.2'
+
+set :application, 'cryptostatus'
+set :repo_url, 'git@github.com:kirillshevch/cryptostatus.git'
+
+set :use_sudo, false
+set :deploy_via, :copy
+set :keep_releases, 2
+
+set :log_level, :debug
+set :pty, true
+
+set :rvm1_ruby_version, 'ruby-2.3.4'
+set :rvm_type, :user
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+
+set :linked_files, ['.env']
+set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle']
+
+namespace :deploy do
+  desc 'create_db'
+  task :create_db do
+    on roles(:app) do
+      invoke 'rvm1:hook'
+      within release_path do
+        execute :bundle, :exec, :"rails db:create RAILS_ENV=#{fetch(:stage)}"
+      end
+    end
+  end
+
+  desc 'Uploads required config files'
+  task :upload_configs do
+    on roles(:all) do
+      upload!(".env.#{fetch(:stage)}", "#{deploy_to}/shared/.env")
+    end
+  end
+
+  desc 'Seeds database'
+  task :seed do
+    on roles(:app) do
+      invoke 'rvm1:hook'
+      within release_path do
+        execute :bundle, :exec, :"rails db:seed RAILS_ENV=#{fetch(:stage)}"
+      end
+    end
+  end
+
+  before 'deploy:migrate', 'deploy:create_db'
+  after :finished, 'deploy:seed'
+  after :finished, 'app:restart'
+end
+
+namespace :app do
+  desc 'Start application'
+  task :start do
+    on roles(:app) do
+      invoke 'rvm1:hook'
+      within "#{fetch(:deploy_to)}/current/" do
+        execute :bundle, :exec, :"puma -C config/puma.rb -e #{fetch(:stage)}"
+      end
+    end
+  end
+
+  desc 'Stop application'
+  task :stop do
+    on roles(:app) do
+      invoke 'rvm1:hook'
+      within "#{fetch(:deploy_to)}/current/" do
+        execute :bundle, :exec, :'pumactl -F config/puma.rb stop'
+      end
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app) do
+      invoke 'rvm1:hook'
+      within "#{fetch(:deploy_to)}/current/" do
+        if test("[ -f #{deploy_to}/current/tmp/pids/puma.pid ]")
+          execute :bundle, :exec, :'pumactl -F config/puma.rb stop'
+        end
+
+        execute :bundle, :exec, :"puma -C config/puma.rb -e #{fetch(:stage)}"
+      end
+    end
+  end
+end
