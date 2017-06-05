3b95d8a9fdb967bb454ae3464e73c03a49591c5d
Ihor Dotsenko <igordocenko1997@gmail.com>
2017-06-05 22:49:43 +0300

    Remove useless import of rails_helper.rb

--- spec/forms/balance_form_spec.rb
+++ spec/forms/balance_form_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe BalanceForm, type: :form do
   let(:user) { create :user }
   let(:params) do
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe UserMailer do
   describe '#take_profit_email' do
     let(:balance) { create(:balance) }
