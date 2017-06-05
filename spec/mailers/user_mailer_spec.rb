076ec3ed137ceb0b76fcacbec374f002fd90cefb
Ihor Dotsenko <igordocenko1997@gmail.com>
2017-06-05 08:52:38 +0300

    Add specs for UserMailer#take_profit_email

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UserMailer do
+  describe '#take_profit_email' do
+    let(:balance) { create(:balance) }
+
+    subject { UserMailer.take_profit_email(balance).deliver }
+
+    it { expect(subject.to  ).to match_array([balance.user.email]) }
+    it { expect(subject.from).to match_array(['no-reply@cryptostat.us']) }
+
+    it { expect(subject.subject     ).to include('Take your profit!') }
+    it { expect(subject.body.encoded).to include('Take your profit!') }
+  end
+end
