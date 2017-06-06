cdb19fd2281575c1cdcf2b9369a37c62481fc8f1
Ihor Dotsenko <igordocenko1997@gmail.com>
2017-06-06 20:49:36 +0300

    Refactor specs for Ability

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,27 +1,25 @@
 describe Ability do
-  let!(:user) { create(:user) }
-  let!(:balance) { create(:balance, user: user) }
-  let!(:new_balance) { build(:balance, user: user) }
-  let!(:second_user) { create(:user) }
-  let!(:second_balance) { create(:balance, user: second_user) }
-  let!(:second_new_balance) { build(:balance, user: second_user) }
-
   subject { Ability.new(user) }
 
-  describe 'Balance' do
-    context 'current user' do
-      it { is_expected.to be_able_to(:read, balance) }
-      it { is_expected.to be_able_to(:update, balance) }
-      it { is_expected.to be_able_to(:destroy, balance) }
-      it { is_expected.to be_able_to(:create, new_balance) }
+  let(:user) { create(:user) }
 
+  describe 'Balance' do
+    context 'belongs to user' do
+      let(:balance) { build(:balance, user: user) }
+
+      it { is_expected.to be_able_to(:create,  balance) }
+      it { is_expected.to be_able_to(:read,    balance) }
+      it { is_expected.to be_able_to(:update,  balance) }
+      it { is_expected.to be_able_to(:destroy, balance) }
     end
 
-    context 'foreign user' do
-      it { is_expected.not_to be_able_to(:read, second_balance) }
-      it { is_expected.not_to be_able_to(:update, second_balance) }
-      it { is_expected.not_to be_able_to(:destroy, second_balance) }
-      it { is_expected.not_to be_able_to(:destroy, second_new_balance) }
+    context 'does not belong to user' do
+      let(:balance) { build(:balance) }
+
+      it { is_expected.not_to be_able_to(:create,  balance) }
+      it { is_expected.not_to be_able_to(:read,    balance) }
+      it { is_expected.not_to be_able_to(:update,  balance) }
+      it { is_expected.not_to be_able_to(:destroy, balance) }
     end
   end
 end
