6742c73ec4c5d9de05c31968fbc8f8a731b33950
Kirill Shevchenko <kirill@rubygarage.org>
2017-06-10 15:12:32 +0300

    move chef to separate repo (#25)

--- chef/site-cookbooks/chef-nginx/recipes/production.rb
+++
@@ -1,7 +0,0 @@
-template "#{node.nginx.dir}/sites-available/cryptostatus.conf" do
-  source 'cryptostatus.conf.erb'
-  mode '0644'
-  notifies :reload, 'service[nginx]', :delayed
-end
-
-nginx_site 'cryptostatus.conf'
