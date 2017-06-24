d99e36157dcf380f957d51d2670a3fed0d9178bb
Kirill Shevchenko <kirill@rubygarage.org>
2017-06-24 12:42:16 +0300

    add mail sender to config (#30)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@cryptostat.us'
+  default from: ENV['MAILER_SENDER']
 
   layout 'mailer'
 end
