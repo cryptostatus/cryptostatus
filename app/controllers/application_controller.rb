a5de3cf16dfbe527761a74cb9152bbe8de9e3576
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2017-08-26 21:04:20 +0300

    Remove include redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@
 require 'application_responder'
 
 class ApplicationController < ActionController::Base
-  include Redirect
-
   self.responder = ApplicationResponder
 
   protect_from_forgery with: :null_session
