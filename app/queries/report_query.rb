5c941d69c1ecfee35d2b92a3af07f7ee5c00c95b
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2017-09-09 20:14:07 +0300

    Update report query

--- app/queries/report_query.rb
+++ app/queries/report_query.rb
@@ -1,23 +1,25 @@
 class ReportQuery
-  def self.max_growth
-    report.order('max_diff desc').limit(1).first
-  end
+  class << self
+    def max_growth
+      report.order('max_diff desc').limit(1).first
+    end
 
-  def self.min_growth
-    report.order('min_diff asc').limit(1).first
-  end
+    def min_growth
+      report.order('min_diff asc').limit(1).first
+    end
 
-  private
+    private
 
-  def self.report
-    @report ||= Currency.select(:name, 'MAX(yesterday_currencies.price) - MAX(currencies.price) as max_diff', 'MIN(yesterday_currencies.price) - MIN(currencies.price) as min_diff')
-                        .joins("INNER JOIN (#{yesterday_currencies.to_sql}) AS yesterday_currencies ON currencies.name = yesterday_currencies.name")
-                        .where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
-                        .group(:name)
-  end
+    def report
+      @report ||= Currency.select(:name, 'MAX(yesterday_currencies.price) - MAX(currencies.price) as max_diff', 'MIN(yesterday_currencies.price) - MIN(currencies.price) as min_diff')
+                          .joins("INNER JOIN (#{yesterday_currencies.to_sql}) AS yesterday_currencies ON currencies.name = yesterday_currencies.name")
+                          .where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
+                          .group(:name)
+    end
 
-  def self.yesterday_currencies
-    range_day = Date.yesterday.beginning_of_day..Date.yesterday.end_of_day
-    @yesterday_currencies ||= Currency.select(:name, :created_at, :price).where(created_at: range_day)
+    def yesterday_currencies
+      range_day = Date.yesterday.beginning_of_day..Date.yesterday.end_of_day
+      @yesterday_currencies ||= Currency.select(:name, :created_at, :price).where(created_at: range_day)
+    end
   end
 end
