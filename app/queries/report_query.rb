d4e9b6078cfcde94e9deaffc42f56cdda0964615
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2017-09-17 12:39:29 +0300

    Fix report query

--- app/queries/report_query.rb
+++ app/queries/report_query.rb
@@ -1,25 +1,40 @@
+# frozen_string_literal: true
+
 class ReportQuery
-  class << self
-    def max_growth
-      report.order('max_diff desc').limit(1).first
-    end
+  attr_reader :yesterday_data, :today_data
 
-    def max_drop
-      report.order('min_diff asc').limit(1).first
-    end
+  def initialize
+    @yesterday_data = fetch_prices(Date.yesterday.beginning_of_day..Date.yesterday.end_of_day)
+    @today_data = fetch_prices((Time.now.utc - 1.minute)..Date.today.end_of_day)
+  end
 
-    private
+  def call
+    diff = growth_diff
 
-    def report
-      @report ||= Currency.select(:name, :price, 'MAX(yesterday_currencies.price) - MAX(currencies.price) as max_diff', 'MIN(yesterday_currencies.price) - MIN(currencies.price) as min_diff')
-                          .joins("INNER JOIN (#{yesterday_currencies.to_sql}) AS yesterday_currencies ON currencies.name = yesterday_currencies.name")
-                          .where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
-                          .group(:name, :price)
-    end
+    growth_name = diff.max_by { |_name, info| info[:max] }[0]
+    drop_name = diff.min_by { |_name, info| info[:min] }[0]
 
-    def yesterday_currencies
-      range_day = Date.yesterday.beginning_of_day..Date.yesterday.end_of_day
-      @yesterday_currencies ||= Currency.select(:name, :created_at, :price).where(created_at: range_day)
+    {
+      max_growth: { name: growth_name, price: today_data[growth_name][:price] },
+      max_drop: { name: drop_name, price: today_data[drop_name][:price] }
+    }
+  end
+
+  private
+
+  def growth_diff
+    yesterday_data.merge(today_data) do |_name, yesterday, today|
+      { max: yesterday[:max] - today[:max], min: yesterday[:min] - today[:min] }
     end
+  end
+
+  def fetch_prices(range_day)
+    data = Currency.select(:name, :price, 'MAX(currencies.price) as max_price', 'MIN(currencies.price) as min_price')
+                   .group(:name, :price)
+                   .where(created_at: range_day)
+
+    data.map do |currency|
+      [currency.name, { max: currency.max_price, min: currency.min_price, price: currency.price }]
+    end.to_h
   end
 end
